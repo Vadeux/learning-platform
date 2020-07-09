a1a93e8f47f129c1e4a1594e4fe4a2a46fc67919
Vadim <bardiervadim97@gmail.com>
2020-07-09 18:38:13 +0300

    'add_custom_field'

--- courses/models.py
+++ courses/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
+from .fields import OrderField
+
 
 # Create your models here.
 class Subject(models.Model):
@@ -35,12 +37,13 @@ class Module(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='modules')
     title = models.CharField(max_length=225)
     description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=['course'])
 
     class Meta:
-        ordering = ['title']
+        ordering = ['order']
 
     def __str__(self):
-        return self.title
+        return '{}. {}'.format(self.order, self.title)
 
 
 class Content(models.Model):
@@ -49,6 +52,10 @@ class Content(models.Model):
                                      limit_choices_to={'models__in': ('text', 'video', 'image', 'file')})
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
+    order = OrderField(blank=True, for_fields=['module'])
+
+    class Meta:
+        ordering = ['order']
 
 
 class ItemBase(models.Model):
