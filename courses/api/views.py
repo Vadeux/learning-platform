fd18b6cb9180a1c611026fd7f6ab2d21b5058fab
Vadim <bardiervadim97@gmail.com>
2020-07-24 15:50:36 +0300

    'api_for_courses_and_enroll_student'

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,7 +1,36 @@
-from rest_framework import generics
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, viewsets
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
+from ..models import Course
 from ..models import Subject
-from .serializers import SubjectSerializer
+from .serializers import SubjectSerializer, CourseSerializer
+
+
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+    @action(detail=True, methods=['post'], authentication_classes=[BasicAuthentication],
+            permission_classes=[IsAuthenticated])
+    def enroll(self, request, *args, **kwargs):
+        course = self.get_object()
+        course.students.add(request.user)
+        return Response({'enrolled': True})
+
+
+# class CourseEnrollView(APIView):
+#     authentication_classes = (BasicAuthentication,)
+#     permission_classes = (IsAuthenticated,)
+#
+#     def post(self, request, pk, format=None):
+#         course = get_object_or_404(Course, pk=pk)
+#         course.students.add(request.user)
+#         return Response({'enrolled': True})
 
 
 class SubjectListView(generics.ListAPIView):
